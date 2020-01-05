4800f3255f9e23f544488ed5932898b3ee0dc97c
RishabRao <rishabpaws@gmail.com>
2020-01-04 19:53:53 -0500

    removed imports that are not required

--- RiseRobot/src/main/java/frc/robot/Robot.java
+++ RiseRobot/src/main/java/frc/robot/Robot.java
@@ -1,9 +1,6 @@
 package frc.robot;
 
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
-
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 /**
