764555a3b2a84e7908e61a4e62f94b612ccd379b
RishabRao <rishabpaws@gmail.com>
2020-01-04 21:50:41 -0500

    added interface

--- RiseRobot/src/main/java/frc/robot/robots/CompDeepSpace.java
+++ RiseRobot/src/main/java/frc/robot/robots/CompDeepSpace.java
@@ -1,18 +1,8 @@
 package frc.robot.robots;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-
-import edu.wpi.first.wpilibj.SpeedControllerGroup;
-
 public class CompDeepSpace {
 
-  private SpeedControllerGroup leftTransmission;
-  private SpeedControllerGroup rightTransmission;
-
   public CompDeepSpace() {
-
-    leftTransmission = new SpeedControllerGroup(new WPI_TalonFX(), new WPI_TalonFX());
-    rightTransmission = new SpeedControllerGroup(new WPI_TalonFX(), new WPI_TalonFX());
 
   }
 }
---
+++ RiseRobot/src/main/java/frc/robot/robots/WaltRobot.java
@@ -0,0 +1,34 @@
+package frc.robot.robots;
+
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
+
+public interface WaltRobot {
+
+    // Ramsete constants
+    double getTrackWidth();
+    double getKBeta();
+    double getKZeta();
+
+    // Velocity controller constants
+    PIDController getLeftPIDController();
+    PIDController getRightPIDController();
+
+    // Shooter feedforward
+    SimpleMotorFeedforward getFlywheelFeedforward();
+
+    // Drivetrain feedforward
+    SimpleMotorFeedforward getDrivetrainFeedforward();
+
+    // Left SpeedController group
+    SpeedControllerGroup getLeftSpeedControllerGroup();
+
+    // right SpeedController group
+    SpeedControllerGroup getRightSpeedControllerGroup();
+
+    double getRpmToMeters();
+
+    void setSpeeds(double leftSpeed, double rightSpeed);
+    void setVoltages(double leftVoltage, double rightVoltage);
+}
