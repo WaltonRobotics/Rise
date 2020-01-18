83f71e30cc6b6331a615d5112569bc2e65d5e519
Russell Newton <rnewtonkids001@gmail.com>
2020-01-18 14:51:23 -0500

    Allow implementation for EnhancedJoystickButton to use dynamic button index

--- RiseRobot/src/main/java/frc/utils/EnhancedJoystickButton.java
+++ RiseRobot/src/main/java/frc/utils/EnhancedJoystickButton.java
@@ -1,62 +1,84 @@
 package frc.utils;
 
+import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import java.util.function.IntSupplier;
 
 /**
- * JoystickButton wrapper class with built in rising and falling edge detection
+ * JoystickButton wrapper class with built in rising and falling edge detection.
+ *
+ * @author Russell Newton, Walton Robotics
  */
 public class EnhancedJoystickButton extends JoystickButton {
 
-    private boolean current;
-    private boolean previous;
+  private final IntSupplier buttonNumberGetter;
+  private final GenericHID m_joystick;
+  private boolean current;
+  private boolean previous;
+
+  /**
+   * Create a new EnhancedJoystickButton on {@code joystick}. Because {@code buttonNumberGetter} is
+   * an IntSupplier, the actual button index can be dynamically set.
+   */
+  public EnhancedJoystickButton(Joystick joystick, IntSupplier buttonNumberGetter) {
+    super(joystick, 0);
+    this.m_joystick = joystick;
+    this.buttonNumberGetter = buttonNumberGetter;
+    whenPressed(new SetValue(true));
+    whenReleased(new SetValue(false));
+  }
+
+  /**
+   * Create a new EnhancedJoystickButton at {@code buttonNumber} on {@code joystick}.
+   */
+  public EnhancedJoystickButton(Joystick joystick, int buttonNumber) {
+    this(joystick, () -> buttonNumber);
+  }
+
+  /**
+   * Returns true if the button has just been pressed.
+   */
+  public boolean isRisingEdge() {
+    return current && !previous;
+  }
+
+  /**
+   * Returns true if the button has just been released.
+   */
+  public boolean isFallingEdge() {
+    return !current && previous;
+  }
+
+  @Override
+  public boolean get() {
+    return m_joystick.getRawButton(buttonNumberGetter.getAsInt());
+  }
+
+  /**
+   * This handles the value setting, because you can only run commands on rising and falling
+   * action.
+   *
+   * @author Russell Newton, Walton Robotics
+   */
+  private class SetValue extends CommandBase {
 
     /**
-     * Create a new EnhancedJoystickButton at {@code buttonNumber} on
-     * {@code joystick}.
+     * Sets the {@code EnhancedJoystickButton} values, as necessary.
      */
-    public EnhancedJoystickButton(Joystick joystick, int buttonNumber) {
-        super(joystick, buttonNumber);
-        whenPressed(new SetValue(true));
-        whenReleased(new SetValue(false));
+    public SetValue(boolean value) {
+      previous = current;
+      current = value;
     }
 
     /**
-     * Returns true if the button has just been pressed.
+     * Stop immediately.
      */
-    public boolean isRisingEdge() {
-        return current && !previous;
+    @Override
+    public boolean isFinished() {
+      return true;
     }
-
-    /**
-     * Returns true if the button has just been released.
-     */
-    public boolean isFallingEdge() {
-        return !current && previous;
-    }
-
-    /**
-     * This handles the value setting, because you can only run commands on rising
-     * and falling action.
-     */
-    private class SetValue extends CommandBase {
-
-        /**
-         * Sets the {@code EnhancedJoystickButton} values, as necessary.
-         */
-        public SetValue(boolean value) {
-            previous = current;
-            current = value;
-        }
-
-        /**
-         * Stop immediately.
-         */
-        @Override
-        public boolean isFinished() {
-            return true;
-        }
-    }
+  }
 
 }
