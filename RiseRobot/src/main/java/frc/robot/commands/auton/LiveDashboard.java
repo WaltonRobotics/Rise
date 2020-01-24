1118146a04fb956a77c8f5c6957b6f80b0cd9eae
vmalepati1 <malepativikas@gmail.com>
2020-01-23 19:34:21 -0500

    added livedashboard setters

--- RiseRobot/src/main/java/frc/robot/commands/auton/LiveDashboard.java
+++ RiseRobot/src/main/java/frc/robot/commands/auton/LiveDashboard.java
@@ -4,6 +4,34 @@ import edu.wpi.first.networktables.NetworkTable;
 public class LiveDashboard {
     private final NetworkTable liveDashboardTable = LiveDashboardTable.getTable("Live_Dashboard");
 
+    public void setRobotX(double robotX) {
+        liveDashboardTable.getEntry("robotX").setDouble(robotX);
+    }
+
+    public void setRobotY(double robotY) {
+        liveDashboardTable.getEntry("robotY").setDouble(robotY);
+    }
+
+    public void setRobotHeading(double robotHeading) {
+        this.robotHeading = robotHeading;
+    }
+
+    public void setFollowingPath(boolean followingPath) {
+        isFollowingPath = followingPath;
+    }
+
+    public void setPathX(double pathX) {
+        this.pathX = pathX;
+    }
+
+    public void setPathY(double pathY) {
+        this.pathY = pathY;
+    }
+
+    public void setPathHeading(double pathHeading) {
+        this.pathHeading = pathHeading;
+    }
+
     private double robotX = liveDashboardTable.getEntry("robotX").getDouble(0.0);
     private double robotY = liveDashboardTable.getEntry("robotY").getDouble(0.0);
     private double robotHeading = liveDashboardTable.getEntry("robotHeading").getDouble(0.0);
