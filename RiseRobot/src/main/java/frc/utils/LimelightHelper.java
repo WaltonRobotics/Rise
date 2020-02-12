f8b02416faf0bffb048a866683905c413252283f
Rishab Rao <rishabpaws@gmail.com>
2020-02-12 12:01:24 -0500

    added annotations and changed units

--- RiseRobot/src/main/java/frc/utils/LimelightHelper.java
+++ RiseRobot/src/main/java/frc/utils/LimelightHelper.java
@@ -3,8 +3,9 @@ package frc.utils;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Units;
+
+import static frc.robot.Robot.currentRobot;
 
 public class LimelightHelper {
     private LimelightHelper() {
@@ -20,19 +21,38 @@ public class LimelightHelper {
     private static double limelightHeight = 10;
     private static double targetHeight = 10;
 
+    /**
+     * @return tx The x angle from target in degrees
+     */
     public static double getTX() {
         return tx.getDouble(0);
     }
+
+    /**
+     * @return ty The y angle from target in degrees
+     */
     public static double getTY() {
         return ty.getDouble(0);
     }
+
+    /**
+     * @return ta The area of the target
+     */
     public static double getTA() {
         return ta.getDouble(0);
     }
+
+    /**
+     * @return tv The number of targets in the field of view
+     */
     public static double getTV() {
         return tv.getDouble(0);
     }
-    public static double getDistance() {
-        return targetHeight - limelightHeight / Math.tan(Units.degreesToRadians(20 + getTY()));
+
+    /**
+     * @return distance The distance to the target in meters
+     */
+    public static double getDistanceMeters() {
+        return Units.inchesToMeters(targetHeight - limelightHeight / Math.tan(Units.degreesToRadians(currentRobot.getMountingAngle() + getTY())));
     }
 }
