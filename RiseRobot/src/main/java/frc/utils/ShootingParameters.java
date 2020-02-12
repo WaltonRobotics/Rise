160ea13151aa55afcdda47ecd6c2ff53139fdf1b
Rishab Rao <rishabpaws@gmail.com>
2020-02-12 12:02:12 -0500

    Create ShootingParameters.java

---
+++ RiseRobot/src/main/java/frc/utils/ShootingParameters.java
@@ -0,0 +1,22 @@
+package frc.utils;
+
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+
+public class ShootingParameters {
+
+    double distance;
+    Rotation2d angle;
+
+    public ShootingParameters(double distance, Rotation2d angle) {
+        this.distance = distance;
+        this.angle = angle;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public Rotation2d getAngle() {
+        return angle;
+    }
+}
