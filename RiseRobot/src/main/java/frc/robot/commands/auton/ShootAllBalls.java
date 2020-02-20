08ff95bcefb0f50a68d372dd635091437bebebb8
Rishab Rao <rishabpaws@gmail.com>
2020-02-20 15:39:21 -0500

    shoots all balls in auto

---
+++ RiseRobot/src/main/java/frc/robot/commands/auton/ShootAllBalls.java
@@ -0,0 +1,52 @@
+package frc.robot.commands.auton;
+
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.utils.LimelightHelper;
+
+import static frc.robot.Robot.intakeConveyor;
+import static frc.robot.Robot.turretShooter;
+import static frc.robot.subsystems.IntakeConveyor.BACK_CONVEYOR_POWER;
+import static frc.robot.subsystems.IntakeConveyor.FRONT_CONVEYOR_POWER;
+
+
+public class ShootAllBalls extends CommandBase {
+
+    private Timer timer;
+    private double totalTime;
+    private int rpm;
+
+    public ShootAllBalls(double time) {
+        timer = new Timer();
+        totalTime = time;
+        rpm = -1;
+    }
+
+    public ShootAllBalls(double time, int rpmFlyWheel) {
+        timer = new Timer();
+        totalTime = time;
+        rpm = rpmFlyWheel;
+    }
+
+    @Override
+    public void initialize() {
+        timer.start();
+
+        if(rpm != -1) {
+            rpm = (int)turretShooter.estimateTargetSpeed(LimelightHelper.getDistanceMeters());
+        }
+    }
+
+    @Override
+    public void execute() {
+        intakeConveyor.setFrontConveyorMotorOutput(FRONT_CONVEYOR_POWER);
+        intakeConveyor.setBackConveyorMotorOutput(BACK_CONVEYOR_POWER);
+        turretShooter.setFlywheelOutput(TalonFXControlMode.Velocity, rpm);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return timer.get() == totalTime;
+    }
+}
